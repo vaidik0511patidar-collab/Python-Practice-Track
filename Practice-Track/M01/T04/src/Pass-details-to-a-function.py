bacfa84ec44e06ee3a29686299c1dfecd5d8cfb9
Vaidik Patidar <vaidik0511patidar@gmail.com>
2026-08-11 13:22:43 +0530

    Understanding parameters and arguments in functions

---
+++ Practice-Track/M01/T04/src/Pass-details-to-a-function.py
@@ -0,0 +1,10 @@
+def display_student(name, course):
+    # Write your code here
+    print(f"Student: {name}")
+    print(f"Course: {course}")
+
+student_name = input()
+course_name = input()
+
+# Call the function and pass the inputs as arguments
+display_student(student_name, course_name)
