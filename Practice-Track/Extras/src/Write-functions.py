673e32aca3698a34b9698928e8e384d5f8b6801a
Vaidik Patidar <vaidik0511patidar@gmail.com>
2026-08-18 10:58:44 +0530

    Write functions to calculate addition and subtraction and return the values

---
+++ Practice-Track/Extras/src/Write-functions.py
@@ -0,0 +1,14 @@
+def sum(num1,num2):
+    return num1 + num2
+
+def subtract(num1,num2):
+    return num1 - num2
+
+num1 = 20
+num2 = 15
+
+res1 = sum(num1,num2)
+res2 = subtract(num1,num2)
+
+print("Sum is:",res1)
+print("Subtraction is:",res2)
